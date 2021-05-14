168dc93a64986ab0ae2f0150592007224b6c0bc3
tutu <1571905344@qq.com>
2021-05-14 21:53:42 +0800

    第一次学习

---
+++ two-sum/TwoSum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            int a=nums[i];
+            for(int j=1;j<(nums.length/2);j++){
+                int b=nums[j];
+                if((a+b)==target){
+                    System.out.println("["+i+","+j+"]");
+                }
+            }
+        }
+	return null;
+    }
+}
+public class TwoSum{
+	public static void main(String args[]){
+		Solution solution = new Solution();
+		int [] nums={1,2,4,5,6,7,8,9};
+		solution.twoSum(nums,10);
+	}
+}
